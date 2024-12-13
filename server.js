78dfe51263f86bd721c670eedce61f41f662bf09
marcusryden92 <marcus.ryden1992@gmail.com>
2024-12-13 10:04:39 +0100

    --

--- server.js
+++ server.js
@@ -1,86 +1,37 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-
-const logEvents = require("./logEvents");
-const EventEmitter = require("events");
-class Emitter extends EventEmitter {}
-
-// initialize object
-const myEmitter = new Emitter();
-
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+// middleware for handling urlencoded data
+app.use(express.urlencoded({ extended: false }));
 
-  const extension = path.extname(req.url);
+// middleware for handling json
+app.use(express.json());
 
-  let contentType;
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
 
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    default:
-      contentType = "text/html";
-  }
-
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "views", req.url)
-      : path.join(__dirname, req.url);
-
-  // makes the .html extension not required in the browser
-  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    // serve the file
-  } else {
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-      // server 404 response
-    }
-  }
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
 
-/* // add listener for the log events
-myEmitter.on("log", (msg) => logEvents(msg));
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(path.join(301, "/new-page.html"));
+});
 
-setTimeout(() => {
-  // emit event
-  myEmitter.emit("log", "Log event emitted!");
-}, 2000);
- */
+// Route handlers
+app.get("/hello(.html)?", (req, res, next) => {
+  console.log("attem");
+  next();
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
